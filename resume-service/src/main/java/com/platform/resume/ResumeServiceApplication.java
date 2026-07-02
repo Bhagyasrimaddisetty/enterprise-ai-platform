55aa6096d411cd0cf9f7b634dcb6225a172e9f13
MADDISETTY BHAGYA SRI <166383988+Bhagyasrimaddisetty@users.noreply.github.com>
2026-07-02 21:29:15 +0530

    Update ResumeServiceApplication.java

--- resume-service/src/main/java/com/platform/resume/ResumeServiceApplication.java
+++ resume-service/src/main/java/com/platform/resume/ResumeServiceApplication.java
@@ -1,10 +1,14 @@
 package com.platform.resume;
 
+import com.platform.resume.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class ResumeServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ResumeServiceApplication.class, args);
     }
